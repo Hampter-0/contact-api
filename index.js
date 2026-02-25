e977fdcc3252698797cf11ad3987395540850280
Hampter-0 <stijn09dijkhuis@gmail.com>
2026-02-26 00:04:20 +0100

    add axios and express-rate-limit (5 req per min) and dependencies

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const rateLimit = require("express-rate-limit");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 5,
+    message: { error: "Too many requests" }
+});
+
+app.use("/contact", limiter);
 app.use(express.json());
 app.use(cors({
     origin: "https://portfolio.hampternom.nl", // for usage replace with your own domain :)
