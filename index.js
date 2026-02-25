4fa48298bd9d000693d05538d59ac7548826110f
Hampter-0 <stijn09dijkhuis@gmail.com>
2026-02-25 11:40:00 +0100

    add dotenv config and cors

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: "https://portfolio.hampternom.nl", // for usage replace with your own domain :)
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
